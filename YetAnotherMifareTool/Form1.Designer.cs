1570f378c910297a9d36fdaa9a540737ebd242bb
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2023-10-17 15:07:00 +0200

    - initial commit

---
+++ YetAnotherMifareTool/Form1.Designer.cs
@@ -0,0 +1,37 @@
+﻿namespace YetAnotherMifareTool
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            MaximizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Yet Another Mifare Tool";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
