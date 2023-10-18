0d1df12577d4567c6bec42d17522ab7ce54f5ef1
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2023-10-18 09:01:04 +0200

    - update version info

--- YetAnotherMifareTool/Form1.cs
+++ YetAnotherMifareTool/Form1.cs
@@ -6,7 +6,9 @@ namespace YetAnotherMifareTool
         {
             InitializeComponent();
 
-            Text += $" v{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}{ThisAssembly.Git.SemVer.DashLabel}";
+            Text += ThisAssembly.Git.Tag.Equals(ThisAssembly.Git.BaseTag) && !string.IsNullOrEmpty(ThisAssembly.Git.Tag)
+                ? $" v{ThisAssembly.Git.SemVer.Major}.{ThisAssembly.Git.SemVer.Minor}.{ThisAssembly.Git.SemVer.Patch}{ThisAssembly.Git.SemVer.DashLabel}"
+                : $" ({ThisAssembly.Git.Commit})";
         }
     }
 }
