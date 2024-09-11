a7d4fe8bdab3eae7e1d14f61780f321490bf3e39
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2024-09-11 21:53:34 +0200

    - add property HasSignature in class Toy

--- YetAnotherMifareTool/Models/Toy.cs
+++ YetAnotherMifareTool/Models/Toy.cs
@@ -7,5 +7,7 @@
         public ushort Variant { get; set; }
         public string Name { get; set; }
         public byte[] Data { get; set; }
+        public bool HasSignature
+            => Id != 699 && Variant >> 12 == 5;
     }
 }
